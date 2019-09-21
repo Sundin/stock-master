de988fee51f48745f47af1f090c9b3d454b2e529
Gustav Sundin <sundin666@gmail.com>
2019-09-21 20:28:36 +0200

    gitignore user portfolio data

--- src/Portfolio/dataRetriever.js
+++ src/Portfolio/dataRetriever.js
@@ -1,46 +1,5 @@
 import { getStockWithAmount } from "../avanza";
-
-const basePortfolioStrategy = {
-  minShare: 25,
-  maxShare: 50,
-  minNumberOfStocks: 8,
-  maxNumberOfStocks: 12,
-  minShareOfEachShareInPortfolio: 5,
-  maxShareOfEachShareInPortfolio: 20
-};
-
-const portfolios = [
-  {
-    id: "1",
-    name: "Portfolio 1",
-    stocks: [
-      {
-        id: "5261",
-        amount: 1
-      },
-      {
-        id: "5332",
-        amount: 1
-      }
-    ],
-    strategy: basePortfolioStrategy
-  },
-  {
-    id: "2",
-    name: "Portfolio 2",
-    stocks: [
-      {
-        id: "5261",
-        amount: 1
-      },
-      {
-        id: "5332",
-        amount: 1
-      }
-    ],
-    strategy: basePortfolioStrategy
-  }
-];
+import portfolios from "../data/portfolioData";
 
 export function getPortfolios() {
   return new Promise((resolve, reject) => {
---
+++ src/examplePortfolioData.js
@@ -0,0 +1,43 @@
+const basePortfolioStrategy = {
+  minShare: 25,
+  maxShare: 50,
+  minNumberOfStocks: 8,
+  maxNumberOfStocks: 12,
+  minShareOfEachShareInPortfolio: 5,
+  maxShareOfEachShareInPortfolio: 20
+};
+
+const examplePortfolios = [
+  {
+    id: "1",
+    name: "Portfolio 1",
+    stocks: [
+      {
+        id: "5261",
+        amount: 1
+      },
+      {
+        id: "5332",
+        amount: 1
+      }
+    ],
+    strategy: basePortfolioStrategy
+  },
+  {
+    id: "2",
+    name: "Portfolio 2",
+    stocks: [
+      {
+        id: "5261",
+        amount: 1
+      },
+      {
+        id: "5332",
+        amount: 1
+      }
+    ],
+    strategy: basePortfolioStrategy
+  }
+];
+
+export default examplePortfolios;
