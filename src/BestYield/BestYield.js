25b06c8cd4129b1e7d9b9eea556dad877f922f5c
Gustav Sundin <sundin666@gmail.com>
2019-09-23 21:05:14 +0200

    Split out StockTable component

--- src/BestYield/BestYield.js
+++ src/BestYield/BestYield.js
@@ -1,20 +1,8 @@
 import React from "react";
+import StockTable from "../components/StockTable";
 import { getInterestingYieldStocks } from "./dataRetriever";
-import {
-  yieldIsGood,
-  yieldIsVeryGood,
-  peIsGood,
-  peIsVeryGood,
-  volatilityIsGood,
-  volatilityIsBad,
-  volatilityIsVeryGood,
-  volatilityIsVeryBad,
-  peIsVeryBad
-} from "./stockIndicators";
-import { getAllOwnedStocks } from "../Portfolio/portfolioUtils";
-import { stockIsOwned } from "../stockUtils";
 
-const classNames = require("classnames");
+import { getAllOwnedStocks } from "../Portfolio/portfolioUtils";
 
 class BestYield extends React.Component {
   state = {
@@ -52,82 +40,13 @@ class BestYield extends React.Component {
     return (
       <div>
         <h1>Intressanta utdelningsaktier</h1>
-        <table>
-          <thead>
-            <tr>
-              <th width="15%">ID</th>
-              <th width="40%">Aktie</th>
-              <th width="15%">P/E</th>
-              <th width="15%">Direktavkastning</th>
-              <th width="15%">Volatilitet</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.stocks.map(stockData => {
-              const owned = stockIsOwned(stockData.id, this.state.ownedStocks);
-              return (
-                <Stock stockData={stockData} key={stockData.id} owned={owned} />
-              );
-            })}
-          </tbody>
-        </table>
+        <StockTable
+          stocks={this.state.stocks}
+          ownedStocks={this.state.ownedStocks}
+        />
       </div>
     );
   }
-}
-
-function Stock(props) {
-  const { stockData, owned } = props;
-  const { priceEarningsRatio, directYield, volatility } = stockData;
-
-  return (
-    <tr key={stockData.id}>
-      <td
-        className={classNames({
-          owned: owned
-        })}
-      >
-        {stockData.id}
-      </td>
-      <td
-        className={classNames({
-          owned: owned
-        })}
-      >
-        {stockData.name}
-      </td>
-      <td
-        className={classNames({
-          good: peIsGood(stockData),
-          veryGood: peIsVeryGood(stockData),
-          veryBad: peIsVeryBad(stockData),
-          owned: owned
-        })}
-      >
-        {priceEarningsRatio}
-      </td>
-      <td
-        className={classNames({
-          good: yieldIsGood(stockData),
-          veryGood: yieldIsVeryGood(stockData),
-          owned: owned
-        })}
-      >
-        {directYield}
-      </td>
-      <td
-        className={classNames({
-          good: volatilityIsGood(stockData),
-          veryGood: volatilityIsVeryGood(stockData),
-          bad: volatilityIsBad(stockData),
-          veryBad: volatilityIsVeryBad(stockData),
-          owned: owned
-        })}
-      >
-        {volatility}
-      </td>
-    </tr>
-  );
 }
 
 export default BestYield;
---
+++ src/components/StockTable.js
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  yieldIsGood,
+  yieldIsVeryGood,
+  peIsGood,
+  peIsVeryGood,
+  volatilityIsGood,
+  volatilityIsBad,
+  volatilityIsVeryGood,
+  volatilityIsVeryBad,
+  peIsVeryBad
+} from "./stockIndicators";
+import { stockIsOwned } from "../stockUtils";
+
+const classNames = require("classnames");
+
+function StockTable(props) {
+  const { stocks, ownedStocks } = props;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th width="15%">ID</th>
+          <th width="40%">Aktie</th>
+          <th width="15%">P/E</th>
+          <th width="15%">Direktavkastning</th>
+          <th width="15%">Volatilitet</th>
+        </tr>
+      </thead>
+      <tbody>
+        {stocks.map(stockData => {
+          const owned = stockIsOwned(stockData.id, ownedStocks);
+          return (
+            <Stock stockData={stockData} key={stockData.id} owned={owned} />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+function Stock(props) {
+  const { stockData, owned } = props;
+  const { priceEarningsRatio, directYield, volatility } = stockData;
+
+  return (
+    <tr key={stockData.id}>
+      <td
+        className={classNames({
+          owned: owned
+        })}
+      >
+        {stockData.id}
+      </td>
+      <td
+        className={classNames({
+          owned: owned
+        })}
+      >
+        {stockData.name}
+      </td>
+      <td
+        className={classNames({
+          good: peIsGood(stockData),
+          veryGood: peIsVeryGood(stockData),
+          veryBad: peIsVeryBad(stockData),
+          owned: owned
+        })}
+      >
+        {priceEarningsRatio}
+      </td>
+      <td
+        className={classNames({
+          good: yieldIsGood(stockData),
+          veryGood: yieldIsVeryGood(stockData),
+          owned: owned
+        })}
+      >
+        {directYield}
+      </td>
+      <td
+        className={classNames({
+          good: volatilityIsGood(stockData),
+          veryGood: volatilityIsVeryGood(stockData),
+          bad: volatilityIsBad(stockData),
+          veryBad: volatilityIsVeryBad(stockData),
+          owned: owned
+        })}
+      >
+        {volatility}
+      </td>
+    </tr>
+  );
+}
+
+export default StockTable;
