306a1e44a65666ffaf662f3ae37394a1c10b199d
Gustav Sundin <sundin666@gmail.com>
2019-11-06 11:58:46 +0100

    Extract translate method

--- src/components/StockTable.js
+++ src/components/StockTable.js
@@ -11,59 +11,9 @@ import {
   peIsVeryBad
 } from "./stockIndicators";
 import { stockIsOwned } from "../stockUtils";
-import {
-  OPERATING_MARGIN,
-  REVENUE_PER_SHARE,
-  REVENUE_PER_EMPLOYEE,
-  REVENUE,
-  NUMBER_OF_EMPLOYEES,
-  VOLATILITY,
-  DIRECT_YIELD,
-  PRICE_SALES_RATIO,
-  PRICE_EARNINGS_RATIO,
-  TOTAL_ASSETS,
-  EARNINGS_PER_SHARE,
-  PRICE_BOOK_VALUE,
-  SOLIDITY,
-  MARKET_CAP
-} from "../constants";
+import { translate } from "../translate";
 
 const classNames = require("classnames");
-
-function translate(key) {
-  switch (key) {
-    case PRICE_EARNINGS_RATIO:
-      return "P/E";
-    case PRICE_SALES_RATIO:
-      return "P/S";
-    case DIRECT_YIELD:
-      return "Direktavkastning (%)";
-    case VOLATILITY:
-      return "Volatilitet";
-    case NUMBER_OF_EMPLOYEES:
-      return "Anställda";
-    case REVENUE:
-      return "Omsättning";
-    case REVENUE_PER_EMPLOYEE:
-      return "Omsättning / anställd";
-    case REVENUE_PER_SHARE:
-      return "Omsättning / aktie";
-    case OPERATING_MARGIN:
-      return "Rörelsemarginal (%)";
-    case TOTAL_ASSETS:
-      return "Tillgångar";
-    case EARNINGS_PER_SHARE:
-      return "EPS";
-    case PRICE_BOOK_VALUE:
-      return "P/B";
-    case SOLIDITY:
-      return "Soliditet (%)";
-    case MARKET_CAP:
-      return "Börsvärde";
-    default:
-      return "";
-  }
-}
 
 function StockTable(props) {
   const { stocks, ownedStocks, sortKey, columnsToShow } = props;
