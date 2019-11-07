13f344409eaf58c2305f54ec9e1d6e1917ea4e83
Gustav Sundin <sundin666@gmail.com>
2019-11-07 11:28:33 +0100

    Add unit tests for formatAllFields and solved some issues

---
+++ src/stockDetails/formatAllFields.test.js
@@ -0,0 +1,123 @@
+import { formatAllFields } from "./stockDetails";
+
+it("formatAllFieldsShouldIgnoreNonNumericFields", () => {
+  const input = {
+    id: "1234",
+    year: 2018,
+    currency: "USD"
+  };
+
+  const expectedResult = {
+    id: "1234",
+    year: 2018,
+    currency: "USD"
+  };
+  expect(formatAllFields(input)).toEqual(expectedResult);
+});
+
+it("formatAllFieldsShouldFormatBillionsCorrectly", () => {
+  const input = {
+    a: 1000000000,
+    b: 2000000000,
+    c: 3333000000,
+    d: 4444444444,
+    e: 5555555555
+  };
+
+  const expectedResult = {
+    a: "1 miljarder",
+    b: "2 miljarder",
+    c: "3.33 miljarder",
+    d: "4.44 miljarder",
+    e: "5.56 miljarder"
+  };
+  expect(formatAllFields(input)).toEqual(expectedResult);
+});
+
+it("formatAllFieldsShouldFormatMillionsCorrectly", () => {
+  const input = {
+    a: 1000000,
+    b: 2000000,
+    c: 3333000,
+    d: 4444444,
+    e: 5555555
+  };
+
+  const expectedResult = {
+    a: "1 miljoner",
+    b: "2 miljoner",
+    c: "3.33 miljoner",
+    d: "4.44 miljoner",
+    e: "5.56 miljoner"
+  };
+  expect(formatAllFields(input)).toEqual(expectedResult);
+});
+
+it("formatAllFieldsShouldFormatThousandsCorrectly", () => {
+  const input = {
+    a: 1000,
+    b: 2000,
+    c: 3333,
+    d: 4444,
+    e: 5555,
+    f: 100000,
+    g: 1000.0,
+    h: 2222.9,
+    i: 1111.1,
+    j: 111111
+  };
+
+  const expectedResult = {
+    a: "1 000",
+    b: "2 000",
+    c: "3 333",
+    d: "4 444",
+    e: "5 555",
+    f: "100 000",
+    g: "1 000",
+    h: "2 223",
+    i: "1 111",
+    j: "111 111"
+  };
+  expect(formatAllFields(input)).toEqual(expectedResult);
+});
+
+it("formatAllFieldsShouldFormatSmallNumbersCorrectly", () => {
+  const input = {
+    a: 999,
+    b: 998.9,
+    c: 0,
+    d: 5,
+    e: 5.55555,
+    f: 999.9
+  };
+
+  const expectedResult = {
+    a: "999",
+    b: "999",
+    c: "0.00",
+    d: "5.00",
+    e: "5.56",
+    f: "1 000"
+  };
+  expect(formatAllFields(input)).toEqual(expectedResult);
+});
+
+it("formatAllFieldsShouldFormatNegativeNumbersCorrectly", () => {
+  const input = {
+    a: -1000000000,
+    b: -2000000000,
+    c: -3333000,
+    d: -4444,
+    e: -555
+  };
+
+  const expectedResult = {
+    a: "-1 miljarder",
+    b: "-2 miljarder",
+    c: "-3.33 miljoner",
+    d: "-4 444",
+    e: "-555"
+  };
+  expect(formatAllFields(input)).toEqual(expectedResult);
+});
--- src/stockDetails/stockDetails.js
+++ src/stockDetails/stockDetails.js
@@ -123,7 +123,7 @@ function calculateKPIs(inputData, inputBasicData) {
   return stockDetails;
 }
 
-function formatAllFields(inputData) {
+export function formatAllFields(inputData) {
   let stockDetails = { ...inputData };
 
   Object.keys(stockDetails).forEach(key => {
@@ -148,13 +148,32 @@ function formatNumber(number) {
   if (isNaN(number) || typeof number === "string") {
     return number;
   }
-  if (number >= BILLION) {
-    return number.toPrecision(3) / BILLION + " miljarder";
-  } else if (number >= MILLION) {
-    return number.toPrecision(3) / MILLION + " miljoner";
-  } else if (number >= THOUSAND) {
-    return number.toPrecision(3) / THOUSAND + " 000";
+
+  let returnValue = "";
+  const negative = number < 0;
+  if (negative) {
+    number = number * -1;
   }
 
-  return number.toPrecision(3);
+  if (number >= BILLION) {
+    returnValue = number.toPrecision(3) / BILLION + " miljarder";
+  } else if (number >= MILLION) {
+    returnValue = number.toPrecision(3) / MILLION + " miljoner";
+  } else if (number >= THOUSAND) {
+    returnValue = numberWithSpaces(number.toFixed(0));
+  } else {
+    returnValue = number.toPrecision(3).replace(".00e+3", " 000");
+  }
+
+  if (negative) {
+    returnValue = "-" + returnValue;
+  }
+
+  return returnValue;
+}
+
+function numberWithSpaces(x) {
+  var parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return parts.join(".");
 }
