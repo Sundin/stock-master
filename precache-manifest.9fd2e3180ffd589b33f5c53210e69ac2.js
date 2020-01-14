a311b902658d5aa511b2d6bb3fd96cf1d06c12e8
Gustav Sundin <sundin666@gmail.com>
2020-01-14 20:40:05 +0100

    Updates

---
+++ precache-manifest.9fd2e3180ffd589b33f5c53210e69ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e3dee9301388d768d89fe405ccb1bb",
+    "url": "/bissenisse/index.html"
+  },
+  {
+    "revision": "329e5d06430bb191e88c",
+    "url": "/bissenisse/static/css/main.40fa71d0.chunk.css"
+  },
+  {
+    "revision": "603dc9409b2ce0ff901f",
+    "url": "/bissenisse/static/js/2.75922118.chunk.js"
+  },
+  {
+    "revision": "329e5d06430bb191e88c",
+    "url": "/bissenisse/static/js/main.ec755086.chunk.js"
+  },
+  {
+    "revision": "7e244d18f1dc33de4127",
+    "url": "/bissenisse/static/js/runtime-main.e916fae7.js"
+  }
+]);
