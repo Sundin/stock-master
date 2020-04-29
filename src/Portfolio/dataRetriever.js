d4692484efc68446cd311a753102de78056283b4
Gustav Sundin <sundin666@gmail.com>
2020-04-29 15:59:29 +0200

    Clean up unused code

--- src/Portfolio/dataRetriever.js
+++ src/Portfolio/dataRetriever.js
@@ -1,4 +1,3 @@
-import { getStockWithAmount } from "../avanza";
 // import portfolios from "../data/portfolioData";
 import portfolios from "./portfolioData";
 import { getStockValue } from "./portfolioUtils";
@@ -6,27 +5,27 @@ import { getStockValue } from "./portfolioUtils";
 export function getPortfolios() {
   return new Promise((resolve, reject) => {
     let promises = [];
-    portfolios.forEach(portfolio => {
-      promises.push(getStocksInPortfolio(portfolio));
+    portfolios.forEach((portfolio) => {
+      // promises.push(getStocksInPortfolio(portfolio));
     });
-    Promise.all(promises).then(returnData => {
+    Promise.all(promises).then((returnData) => {
       resolve(returnData);
     });
   });
 }
 
-function getStocksInPortfolio(portfolio) {
-  let returnData = portfolio;
-  return new Promise((resolve, reject) => {
-    let promises = [];
-    portfolio.stocks.forEach(stock => {
-      promises.push(getStockWithAmount(stock));
-    });
-    Promise.all(promises).then(stocks => {
-      returnData.stocks = stocks.sort((a, b) => {
-        return getStockValue(b) - getStockValue(a);
-      });
-      resolve(returnData);
-    });
-  });
-}
+// function getStocksInPortfolio(portfolio) {
+//   let returnData = portfolio;
+//   return new Promise((resolve, reject) => {
+//     let promises = [];
+//     portfolio.stocks.forEach(stock => {
+//       promises.push(getStockWithAmount(stock));
+//     });
+//     Promise.all(promises).then(stocks => {
+//       returnData.stocks = stocks.sort((a, b) => {
+//         return getStockValue(b) - getStockValue(a);
+//       });
+//       resolve(returnData);
+//     });
+//   });
+// }
