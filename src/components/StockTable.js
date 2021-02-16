9a765a40f1c8f6664eff2c3e037bb0a690e90687
Gustav Sundin <sundin666@gmail.com>
2021-02-16 15:55:33 +0100

    Convert StockTable to a React component

--- src/components/StockTable.js
+++ src/components/StockTable.js
@@ -17,61 +17,63 @@ import {
 } from "../constants";
 import Stock from "./Stock";
 
-function StockTable(props) {
-  const { stocks, ownedStocks, sortKey, type, reportType } = props;
+class StockTable extends React.Component {
+  render() {
+    const { stocks, ownedStocks, sortKey, type, reportType } = this.props;
 
-  if (sortKey) {
-    stocks.sort((a, b) => {
-      return b[sortKey] - a[sortKey];
-    });
-  }
-
-  let columnsToShow = props.columnsToShow;
-
-  if (reportType === QUARTERLY_REPORT) {
-    columnsToShow = columnsToShow.concat([OLLE]);
-  }
-
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th width="40%">{getMainColumn(type)}</th>
-            {columnsToShow.map((column) => {
+    if (sortKey) {
+      stocks.sort((a, b) => {
+        return b[sortKey] - a[sortKey];
+      });
+    }
+  
+    let columnsToShow = this.props.columnsToShow;
+  
+    if (reportType === QUARTERLY_REPORT) {
+      columnsToShow = columnsToShow.concat([OLLE]);
+    }
+  
+    return (
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th width="40%">{getMainColumn(type)}</th>
+              {columnsToShow.map((column) => {
+                return (
+                  <th width="15%" key={column} data-tip={tooltip(column)} onClick={() => console.log(column)}>
+                    {translate(column)}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {stocks.map((stockData) => {
+              const owned = stockIsOwned(stockData.id, ownedStocks);
               return (
-                <th width="15%" key={column} data-tip={tooltip(column)} onClick={() => console.log(column)}>
-                  {translate(column)}
-                </th>
+                <Stock
+                  stockData={
+                    reportType === QUARTERLY_REPORT
+                      ? { ...stockData, ...stockData.latestInterimReport }
+                      : { ...stockData, ...stockData.latestAnnualReport }
+                  }
+                  key={stockData.id}
+                  owned={owned}
+                  columnsToShow={columnsToShow}
+                  showSingleStock={this.props.showSingleStock}
+                  handleClickReport={this.props.handleClickReport}
+                  type={type}
+                  reportType={reportType}
+                />
               );
             })}
-          </tr>
-        </thead>
-        <tbody>
-          {stocks.map((stockData) => {
-            const owned = stockIsOwned(stockData.id, ownedStocks);
-            return (
-              <Stock
-                stockData={
-                  reportType === QUARTERLY_REPORT
-                    ? { ...stockData, ...stockData.latestInterimReport }
-                    : { ...stockData, ...stockData.latestAnnualReport }
-                }
-                key={stockData.id}
-                owned={owned}
-                columnsToShow={columnsToShow}
-                showSingleStock={props.showSingleStock}
-                handleClickReport={props.handleClickReport}
-                type={type}
-                reportType={reportType}
-              />
-            );
-          })}
-        </tbody>
-      </table>
-      <ReactTooltip />
-    </>
-  );
+          </tbody>
+        </table>
+        <ReactTooltip />
+      </>
+    );
+  }
 }
 
 function getMainColumn(tableType) {
